9786156b91e88dbe9afb1d44647a81768252b05d
Pranavkaware <140547918+Pranavkaware@users.noreply.github.com>
2025-09-04 13:46:09 +0530

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,4 @@
+create database bankproj;
+use bankproj;
+create table users(id int auto_increment primary key, uname varchar(100), uemail varchar(100),upass varchar(100), upassw varchar(100));
+select * from users;
